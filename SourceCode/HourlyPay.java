df6da8447715cab6b4282b042897ce3d9c0a5aec
0-5-blood-prince <albusdumbledore8918@gmail.com>
2020-05-09 17:13:24 +0530

    Implemented HourlyPay which follows interface PAymentInfo 	It has a Hashmap of Date and hours and field hourlyrate 	Methods: gethours,addhours,UpdateHourlyRate

---
+++ SourceCode/HourlyPay.java
@@ -0,0 +1,42 @@
+// import org.joda.time.LocalDate;
+import java.util.*;
+import java.time.LocalDate;
+public class HourlyPay implements PaymentInfo
+{
+	private double hourlyrate;
+	private HashMap <LocalDate , Double> HourMap;
+	HourlyPay(double hourlyrate)
+	{
+		this.hourlyrate = hourlyrate;
+		// this.commissionrate = commissionrate;
+		this.HourMap = new HashMap <LocalDate , Double>();
+	}  
+	
+	public void UpdateHourlyrate(double rate)
+	{
+		this.hourlyrate = rate;
+	}
+	public void UpdateHours(LocalDate d,double hours)
+	{
+		HourMap.put(d,new Double(hours));
+	}
+	public Double getHours(LocalDate d)
+	{
+		return HourMap.get(d);
+	}
+	//Interface Methods
+	public int gettype()
+	{
+		return 1;
+	}
+	public Boolean isPay(LocalDate d)
+	{
+		return d.getDayOfWeek().getValue()==5; 
+	}
+	public double calculatePay(LocalDate d)
+	{
+		return 0;
+	}
+
+
+}
