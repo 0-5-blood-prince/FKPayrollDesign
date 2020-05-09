736cd77b5be559fb3319d83a069e745194342554
0-5-blood-prince <albusdumbledore8918@gmail.com>
2020-05-09 17:26:32 +0530

    Implemented AddEmployee Method Which creates Employee object and adds to the database

---
+++ SourceCode/AddEmployee.java
@@ -0,0 +1,45 @@
+public class AddEmployee implements Action{
+	String name;
+	String address;
+	Double hourlyrate;
+	Double salary;
+	Double commissionrate;
+	EmployeeDB db;
+	int type;
+	public AddEmployee(String name ,String address,double hourlyrate,EmployeeDB db)
+	{
+		this.name = name;
+		this.address = address;
+		this.hourlyrate = hourlyrate;
+		this.db = db;
+		this.type = 1;
+	}
+	public AddEmployee(String name, String address , double salary , double commissionrate,EmployeeDB db)
+	{
+		this.name = name;
+		this.address= address;
+		this.salary = salary;
+		this.commissionrate = commissionrate;
+		this.db = db;
+		this.type = 2;
+	}
+	public void Do()
+	{
+		int empid = db.getLatestId();
+		if(type==1)
+		{
+			HourlyPay p = new HourlyPay(hourlyrate);
+			Employee e = new Employee(empid,name,address);
+			e.UpdatePaymentInfo(p);
+			db.addEmployee(empid,e);
+		}
+		else
+		{
+			SalaryPay p = new SalaryPay(salary,commissionrate);
+			Employee e = new Employee(empid,name,address);
+			e.UpdatePaymentInfo(p);
+			db.addEmployee(empid,e);
+		}
+
+	}
+}
