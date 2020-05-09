aa267a696eac036380266f2584f20b6e60ba24e2
0-5-blood-prince <albusdumbledore8918@gmail.com>
2020-05-09 17:49:27 +0530

    Tester updated for Remove Employee

--- SourceCode/Tester.java
+++ SourceCode/Tester.java
@@ -5,14 +5,30 @@ public class Tester
 	public static void TestAddEmployee(EmployeeDB db)
 	{
 		AddEmployee a = new AddEmployee("Mooizz","Hey jude",0.9,db);
-		AddEmployee b = new AddEmployee("ABdul","Hey JUne",10000.0,0.8,db);
+		AddEmployee b = new AddEmployee("ABdul","Hey JUne",90000.0,0.8,db);
+		AddEmployee c = new AddEmployee("buchhu","Hey jude",0.9,db);
+		AddEmployee d = new AddEmployee("Tuss","Hey JUne",1000.0,0,db);
+		AddEmployee e = new AddEmployee("Hass","Hey jude",0.9,db);
+		AddEmployee f = new AddEmployee("Buss","Hey JUne",100,0.8,db);
+		a.Do();
+		b.Do();c.Do();d.Do();e.Do();f.Do();
+	}
+	public static void TestRemoveEmployee(EmployeeDB db)
+	{
+		RemoveEmployee a = new RemoveEmployee(2,db);
+		RemoveEmployee b = new RemoveEmployee(5,db);
 		a.Do();
 		b.Do();
 	}
 	public static void main(String[] args) {
 		EmployeeDB db = new EmployeeDB();
+		//Testing Add
 		TestAddEmployee(db);
 		db.DisplayEmployees();
+		//Testing Remove
+		TestRemoveEmployee(db);
+		db.DisplayEmployees();
+
 
 	}
 }
