d52266bc301faa520102bc10311a49a0b073cb36
0-5-blood-prince <albusdumbledore8918@gmail.com>
2020-05-09 17:05:28 +0530

    Implemented Employee Class with 	Fields 		Id 		name 		address 		ishourly 		issalary 		payment of PaymentInfo interface 	Methods 		Update :name,address,paymentinfo 		Set Hourly,Salary Not Implemented

---
+++ SourceCode/Employee.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Employee{
+	public int id;
+	public String name;
+	public String address;
+	private Boolean isHourlyPay;
+	private Boolean isSalaryPay;
+	public PaymentInfo payment;
+
+	public Employee(int id,String name,String address)
+	{
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	void UpdateName(String name)
+	{
+		this.name = name;
+	}
+	void UpdateAddress(String address)
+	{
+		this.address = address;
+	}
+	void UpdatePaymentInfo(PaymentInfo p)
+	{
+			this.payment = p;
+	}
+	void setHourly()
+	{
+		return;
+	}
+	void setSalary()
+	{
+		return;
+	}
+}
