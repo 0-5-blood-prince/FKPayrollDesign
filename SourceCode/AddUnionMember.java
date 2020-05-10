1ddd93b5352381095f8b3032b1f0e157a0feb9e1
Mooizz <albusdumbledore8918@gmail.com>
2020-05-10 23:04:08 +0530

    Editing

--- SourceCode/AddUnionMember.java
+++ SourceCode/AddUnionMember.java
@@ -1,7 +1,7 @@
 public class AddUnionMember implements Action{
-	int id;
-    double weeklydue;
-	EmployeeDB db;
+	private int id;
+    private double weeklydue;
+	private EmployeeDB db;
 	// int type;
 	public AddUnionMember(int id,double weeklydue,EmployeeDB db)
 	{
@@ -15,34 +15,26 @@ public class AddUnionMember implements Action{
 		//Implement Ispresent DB
 		if(db.isPresent(id))
 		{
-			if(db.isInUnion(id))
+			if (db.isInUnion(id)) {
+				return;
+			}
+			//Modify deduct account in Employee
+			//Opening a deduction Account
+
+			Employee e = db.getEmployee(id);
+			if(e.isDeductOpen())
 			{
-				System.out.println("Error: Given id already in Union");
+				//Can be if Only due is present then update weeklydue
+				Deductions deduct = e.getDeductionInfo();
+				deduct.updateDueRate(weeklydue);
+				e.UpdateDeductionInfo(deduct);
 			}
 			else
 			{
-				//Modify deduct account in Employee
-				//Opening a deduction Account
-				
-				Employee e = db.getEmployee(id);
-				if(e.isDeductOpen())
-				{
-					//Can be if Only due is present then update weeklydue
-					Deductions deduct = e.getDeductionInfo();
-					deduct.updateDueRate(weeklydue);
-					e.UpdateDeductionInfo(deduct);
-				}
-				else
-				{
-					Deductions deduct = new Deductions(weeklydue);
-					e.UpdateDeductionInfo(deduct);
-				}
-				db.addMember(id,e);
+				Deductions deduct = new Deductions(weeklydue);
+				e.UpdateDeductionInfo(deduct);
 			}
-		}
-		else
-		{
-			System.out.println("Error: Given Id Not Found");
+			db.addMember(id,e);
 		}
 	}
 }
