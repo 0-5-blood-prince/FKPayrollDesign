fcdb08555fa02f21f0c15d521159dd0d91c1fdb6
Mooizz <albusdumbledore8918@gmail.com>
2020-05-10 20:42:31 +0530

    Class to Update Due rate of Employee in the Union

---
+++ SourceCode/UpdateDueRate.java
@@ -0,0 +1,20 @@
+public class UpdateDueRate implements UpdateDetails{
+    double rate;
+    EmployeeDB db;
+    int id;
+    UpdateDueRate(int id,double rate,EmployeeDB db)
+    {
+        this.rate = rate;
+        this.db = db;
+        this.id = id;
+    }
+    public void update()
+    {
+        Employee e = db.getEmployee(id);
+        if(db.isInUnion(id))
+        {
+            Deductions d = e.getDeductionInfo();
+            d.updateDueRate(rate);
+        }
+    }
+}
